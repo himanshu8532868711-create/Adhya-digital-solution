cb19f57db6501308736e05a4c252afcf0f903f49
himanshu8532868711-create <himanshu8532868711@gmail.com>
2026-06-11 15:54:10 +0530

    Update robot.tsx
    Signed-off-by: himanshu8532868711-create <himanshu8532868711@gmail.com>

--- src/app/robot.tsx
+++ src/app/robot.tsx
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://humdigitalstudio.com';
+  const baseUrl = 'https://www.aadhyadigitalsolution.com';
 
   return {
     rules: [
